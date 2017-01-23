ee92217baaebef543e0321aa62f88a66055f8997
elliotsayes <elliotsayes@gmail.com>
2017-01-23 21:38:25 +1300

    Take out debugging lines

--- cathash.py
+++ cathash.py
@@ -7,17 +7,18 @@ import base58
 import defs
 import catdb
 
-#web.config.debug = False
+web.config.debug = False
 
 urls = (
     '/', 'index',
-    '/([cd])/(.*)/(.*)', 'single',
+    '/([cd])/(.*)/(.*)', 'raw',
     '/([cd])/([^\/]*)', 'json',
     '/(.*)', 'blank'
 )
 
 def er(t):
-    return t
+    print('er: '+t)
+    return bytes()
 
 class index:
     def GET(self):
@@ -29,7 +30,7 @@ class blank:
 
 code_dict = {'c':defs.Lookups.catalog, 'd':defs.Lookups.discogs}
 
-class single:
+class raw:
     def GET(self,url_code,search_term,format):
         if not format in [f.name for f in defs.Formats]:
             return er('invalid format')
@@ -43,15 +44,18 @@ class single:
         if mh:
             return base58.b58encode(mh[0])
         else:
-            return er('couldn\'t find db entry')
+            return er('no hash for that format')
 
 class json:
     def GET(self,url_code,search_term):
         lookup_enum = code_dict[url_code]
         mh_list = catdb.get_multiple_hash(lookup_enum,search_term)
-        mh_dict = {defs.Formats(x[1]).name:base58.b58encode(x[0]) for x in mh_list}
-        js = _json.dumps(mh_dict)
-        return js
+        mh_dict = {defs.Formats(x[1]).name : base58.b58encode(x[0]) for x in mh_list}
+
+        if mh_dict:
+            return _json.dumps(mh_dict)
+        else:
+            return er('no hash for that code')
 
 if __name__ == "__main__":
     catdb.init_db()
