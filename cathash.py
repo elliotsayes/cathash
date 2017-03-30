4b28845a52c6100f4d4ea75c0bdae844a690196c
elliotsayes <elliotsayes@gmail.com>
2017-03-31 03:30:04 +1300

    Update cathash.py

--- cathash.py
+++ cathash.py
@@ -39,7 +39,7 @@ class raw:
 
         mh = catdb.get_single_hash(lookup_enum,search_term,format_enum)
         if mh:
-            return base58.b58encode(mh[0])
+            return base58.b58encode(bytes(mh[0]))
         else:
             return er('no hash for that format')
 
@@ -47,7 +47,7 @@ class json_dump:
     def GET(self,url_code,search_term):
         lookup_enum = code_dict[url_code]
         mh_list = catdb.get_multiple_hash(lookup_enum,search_term)
-        mh_dict = {defs.Formats(x[1]).name : base58.b58encode(x[0]) for x in mh_list}
+        mh_dict = {defs.Formats(x[1]).name : base58.b58encode(bytes(x[0])) for x in mh_list}
 
         if mh_dict:
             return json.dumps(mh_dict,separators=(',', ':'))
